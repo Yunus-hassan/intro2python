74b22a03bc6c00e6af9a691af38a9b226528e3ec
eMobilis <yunus172004@gmail.com>
2023-06-26 08:59:05 +0300

    added variables and calculator

---
+++ calculate area.py
@@ -0,0 +1,6 @@
+rectangle_length = input("length of rectangle")
+length = int(rectangle_length )
+rectangle_width = input("width of rectangle")
+width = int(rectangle_width)
+Area = width * length
+print(Area)
