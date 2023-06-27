42773e40c40cb62b5aa69a2222778eaf2123c004
eMobilis <yunus172004@gmail.com>
2023-06-27 10:16:21 +0300

    addedd guessing game

---
+++ Betting.py
@@ -0,0 +1,13 @@
+reward = int(input("try your luck, type any number between 1-100"))
+if reward == 5:
+    print("congratulations you have won a phone")
+elif reward == 4:
+   print("congratulations you have won a cash prize")
+elif reward == 3:
+    print("congratulations you have won a car")
+elif reward == 2:
+    print("congratulations you have won a mug")
+elif reward == 1:
+    print("congratulations you have won a trip")
+else:
+    print("try again")
--- score system.py
+++ score system.py
@@ -2,7 +2,7 @@ marks = int(input("Enter your marks"))
 
 if marks < 40:
     print("E")
-elif marks < 50:
+elif marks < 50: 
     print("D")
 elif marks < 60:
     print("C")
