9223e3d282c2ea0601a6d0d2b973ec0b0054fef5
eMobilis <yunus172004@gmail.com>
2023-06-29 08:51:21 +0300

    addedd BMI calculator

--- BMI.py
+++ BMI.py
@@ -3,4 +3,13 @@ height_me = input("Enter your height in metres")
 weight = float(weight_kg)
 height = float(height_me)
 result = weight / (height * height)
-print("your BMI is",result)
+if result <= 18:
+    print(result, "underweight")
+elif result <= 29:
+    print(result, "normal weight")
+elif result <= 34:
+    print(result, "overweight")
+elif result >= 34.1:
+    print(result, "obese")
+else:
+    print("invalid")
