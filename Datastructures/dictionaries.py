4cc23b55867d37708d4e84a914714251c11c610a
eMobilis <yunus172004@gmail.com>
2023-07-03 08:43:18 +0300

    print key and value in dictionary

--- Datastructures/dictionaries.py
+++ Datastructures/dictionaries.py
@@ -3,6 +3,6 @@
 # has order
 # you can modify data in dictionaries
 
-friends_ages = {"roy": 29, "jayden": 20, "mary": 26}
-friends_ages.update({"joe": 69})
-print(friends_ages)
+friends_ages = {"roy": 29, "jayden": 20, "mary": 26, "doe": 30}
+for i , n in friends_ages.items():
+    print(i,n)
--- loops/forloops2.py
+++ loops/forloops2.py
@@ -1,3 +1,3 @@
 friends_ages = {"roy": 29, "jayden": 20, "mary": 26}
-for i , n in friends_ages.items():
+for i, n in friends_ages.items():
     print(i, n)
