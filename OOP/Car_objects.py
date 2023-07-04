460b654fbc9ea0108a91c080c9cf409fd35b7810
eMobilis <yunus172004@gmail.com>
2023-07-04 09:33:23 +0300

    oop new

---
+++ OOP/Car_objects.py
@@ -0,0 +1,8 @@
+from Car import Car
+
+owner1 = Car("toyota", "vitz", "bob", "black")
+owner2 = Car("toyota", "vitz", "joe", "blue")
+owner3 = Car("toyota", "vitz", "bill", "red")
+owner4 = Car("toyota", "vitz", "bem", "green")
+
+print(owner1.owner, owner3.owner, owner4.owner)
